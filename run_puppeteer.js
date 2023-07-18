46d047d2108d121e515694a156b011dceab985c3
lautarograc <lautaro@graciani.ar>
2023-07-18 11:58:34 -0300

    commit

--- run_puppeteer.js
+++ run_puppeteer.js
@@ -1,52 +1,23 @@
-const chromium = require("@sparticuz/chromium")
 const puppeteer = require("puppeteer-core")
+const chromium = require("@sparticuz/chromium")
 
-function delay(time) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time)
-  })
-}
-
-exports.handler = async (event) => {
+exports.handler = async (t) => {
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(),
     headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  })
+  });
 
-  const page = await browser.newPage()
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  })
+  const page = await browser.newPage();
+  await page.goto("https://es.wikipedia.org/wiki/Club_Atl%C3%A9tico_V%C3%A9lez_Sarsfield");
+  const pageTitle = await page.title(); {
+    const selector = '#mw-content-text > div.mw-parser-output > p:nth-child(4)'
+    const text = await page.evaluate((selector) => {
+      return document.querySelector(selector).textContent
+    }, selector)
+    await browser.close();
 
-  await page.goto("https://caulicloud.com/authentication", {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  })
-
-  await page.waitForSelector("#continueWithEmail")
-  await page.click("#continueWithEmail")
-
-  await page.type("#emailInput", "example@email.com")
-  await page.click("#continueLoggingIn")
-
-  await page.waitForSelector("#passwordInput", { visible: true })
-  await page.type("#passwordInput", "wrong_password")
-
-  await page.waitForSelector("#continueLoggingIn", { visible: true })
-  await page.click("#continueLoggingIn")
-
-  await page.waitForSelector("#monitorsList", { visible: true, timeout: 20000 })
-
-  const element = await page.waitForSelector("#monitorsList > li:first-of-type")
-  await element.click()
-
-  await delay(5000)
-
-  await browser.close()
-
-  return event
+    return { statusCode: 200, body: JSON.stringify({ pageTitle, text }) }
+  }
 }
